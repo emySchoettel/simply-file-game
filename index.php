9b1008df3370ef93c2708f5a83817476b19f49b4
emySchoettel <56391268+emySchoettel@users.noreply.github.com>
2021-06-27 17:46:43 +0200

    Etape 1

--- index.php
+++ index.php
@@ -5,29 +5,52 @@
     </head>
     <body>
         <h1>Simply file game</h1>
-        <?php 
-           $path = "fichier_json.json";
-           $data = [ 
-               "name" => "test",
-               "calls" => []
-           ];
-
-           $jsonencode = json_encode($data);
-           $fichier = fopen($path, "w");
-           fputs($fichier, $jsonencode);
-           fclose($fichier);
-
-           //ouvrir le fichier créé 
-           $read = fopen($path, "r");
-           $contenu = fread($read, filesize($path));
-           fclose($read);
-           var_dump($contenu);
-
-        ?>
     </body>
 </html>
+<?php 
 
-<?php
+    date_default_timezone_set('UTC');
 
+    $path = "fichier_json.json";
+    $data = [ 
+        "name" => "test",
+        "calls" => []
+    ];
+    $jsonencode = json_encode($data);
+
+    //ecrire dans le fichier
+    writeInThisFile($path, $jsonencode);
+
+    //ouvrir le fichier créé 
+    $file = openThisFile($path);
+    var_dump($file);
+
+    //ajouter la date du jour dans calls 
+    $date = date("l d F Y");
+    array_push($data["calls"], $date);
+    $jsonencode = json_encode($data);
+    var_dump($jsonencode);
+
+    writeInThisFile($path, $jsonencode);
+
+    $file = openThisFile("./fichier_json.json");
+    var_dump($file);
+
+    function openThisFile($nom)
+    {
+        $read = fopen($nom, "r");
+        $contenu = fread($read, filesize($nom));
+        fclose($read);
+    
+        $jsondecode = json_decode($contenu);
+        return $jsondecode;
+    }
+
+    function writeInThisFile($nom, $data)
+    {
+        $write = fopen($nom, "w");
+        fputs($write, $data);
+        fclose($write);
+    }
 ?>
 
